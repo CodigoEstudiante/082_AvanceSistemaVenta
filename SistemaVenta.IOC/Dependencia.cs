de9d727a600a073d6e0e02597ae7f99e474e2388
jose chambi <jose.chambi@ingrammicro.com>
2023-04-11 19:11:11 -0500

    Agregar archivos de proyecto.

---
+++ SistemaVenta.IOC/Dependencia.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SistemaVenta.DAL.DBContext;
+using Microsoft.EntityFrameworkCore;
+//using SistemaVenta.DAL.Interfaces;
+//using SistemaVenta.DAL.Implementacion;
+//using SistemaVenta.BLL.Interfaces;
+//using SistemaVenta.BLL.Implementacion;
+
+namespace SistemaVenta.IOC
+{
+    public static class Dependencia
+    {
+
+        public static void InyectarDependencia(this IServiceCollection services, IConfiguration Configuration) {
+
+            services.AddDbContext<DBVENTAContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("CadenaSQL"));
+            });
+        
+
+        }
+    }
+}
